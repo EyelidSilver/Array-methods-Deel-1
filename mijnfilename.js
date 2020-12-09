bf9319fd6dd039850275f75bb6a95318c9ba66f1
Aitalina Kirillina <ayta1951@gmail.com>
2020-12-09 17:04:38 +0100

    third commit

--- mijnfilename.js
+++ mijnfilename.js
@@ -2,66 +2,65 @@
  const wowWords = ["nice", "awesome", "tof"];
 
  const addTheWordCool = function (wowWords) {
-     wowWords.push('cool');
-     console.log("Add cool: ", wowWords)
+    wowWords.push('cool'); 
+    return wowWords
  }
 
- addTheWordCool(wowWords);
-
+ console.log(addTheWordCool(wowWords));
 
 
  //B
  const fruits = ['appels', 'peren', 'citroenen'];
 
  const getAmountOfArrayItems = function (fruits) {
-     console.log(fruits.length)
+     return fruits.length
  }
 
- getAmountOfArrayItems(fruits);
+ console.log(getAmountOfArrayItems(fruits));
 
 
  //C
  const benelux = ["Belgie", "Nederland", "Luxemburg"];
 
  const selectBelgiumFromBenelux = function (benelux) {
-     console.log(benelux[0])
+     return benelux[0]
  }
 
- selectBelgiumFromBenelux(benelux);
+ console.log(selectBelgiumFromBenelux(benelux));
 
 
  //D
  const animals = ["Haas", "Cavia", "Kip", "Schildpad"];
 
  const lastElementInArray = function (animals) {
-     console.log(animals[animals.length - 1])
+     return animals[animals.length - 1]
  }
 
- lastElementInArray(animals);
+ console.log(lastElementInArray(animals));
 
 
  //E
  const presidents = ["Trump", "Obama", "Bush", "Clinton"]
 
  const impeachTrumpSlice = function (presidents) {
-     console.log(presidents.slice(1))
+    return presidents.slice(1)
  }
  const impeachTrumpSplice = (presidents) => {
-     console.log(presidents.splice(1))
+    return presidents.splice(1)
  }
 
- impeachTrumpSlice(presidents);
- impeachTrumpSplice(presidents);
+ console.log(impeachTrumpSlice(presidents));
+ console.log(impeachTrumpSplice(presidents));
 
 
  //F
  const wordsArray = ['Winc', 'Academy', 'is', 'leuk', ';-}']
 
  const stringsTogether = function (wordsArray) {
-     console.log(wordsArray.join(' '));
+     return wordsArray.join(' ');
  }
 
- stringsTogether(wordsArray)
+ console.log(stringsTogether(wordsArray))
 
 
  //G
@@ -69,7 +68,7 @@
  const array2 = [4, 5, 6];
 
  const combineArrays = function (combineArrays) {
-     console.log(array1.concat(array2))
+     return array1.concat(array2)
  }
 
- combineArrays(combineArrays)
+ console.log(combineArrays(combineArrays))
